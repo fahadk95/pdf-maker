5ab00ecdf90f75fd9f53b8b124c6f51f4ee4071d
Fahad Mohd Khan <fahad.postbox@gmail.com>
2024-07-11 16:01:01 +0530

    lined pdf

--- main.py
+++ main.py
@@ -11,8 +11,10 @@ for index, row in df.iterrows():
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
-    pdf.line(10,22, 200, 22 )
-    pdf.ln(265)
+    for y in range(20, 298, 10):
+        pdf.line(10, y, 200, y)
+
+    pdf.ln(268)
     # Set the Footer for Master page
     pdf.set_font(family="Times", style="I", size=8)
     pdf.set_text_color(180, 180, 180)
@@ -24,5 +26,7 @@ for index, row in df.iterrows():
         pdf.set_font(family="Times", style="I", size=8)
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+        for y in range(20, 298, 10):
+            pdf.line(10, y, 200, y)
 
 pdf.output('output.pdf')
